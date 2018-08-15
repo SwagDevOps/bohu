2d6009a3248230b045d4cac3653c9362be5adf53
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-16 00:10:14 +0200

    command (lib) introduced

---
+++ lib/bohu/command.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../bohu'
+
+# Config with dot access notation.
+class Bohu::Command
+  autoload :Dialect, "#{__dir__}/command/dialect"
+  autoload :Runner, "#{__dir__}/command/runner"
+
+  # @param [Bohu::Config
+  def initialize(config = Bohu.config)
+    @config = config
+  end
+
+  # Execute a command.
+  #
+  # @param [String|Symbol] name command name
+  # @param [String|Symbol] action action name
+  def call(name, action, **kwargs)
+    transform(name, action, **kwargs).tap do |command|
+      Runner.new(command).call
+    end
+  end
+
+  # Build a command using dialect.
+  #
+  # @param [String|Symbol] name command name
+  # @param [String|Symbol] action action name
+  # @return [Array<String>]
+  def transform(name, action, **kwargs)
+    make_cmd(name, action, **kwargs)
+  end
+
+  protected
+
+  attr_reader :config
+
+  # @param [String|Symbol] name
+  # @param [String|Symbol] action
+  # @return [Array<String>]
+  def make_cmd(name, action, **kwargs)
+    make_args(name, action, **kwargs).tap do |args|
+      self.config.commands.public_send(name).tap do |config|
+        return [config.executable].push(*args)
+      end
+    end
+  end
+
+  # @param [String|Symbol] name
+  # @param [String|Symbol] action
+  # @return [Array<String>]
+  def make_args(name, action, **kwargs)
+    self.config.commands.public_send(name).tap do |config|
+      dialect = Dialect.load(name, config.dialect)
+
+      config.actions.public_send(action).tap do |options|
+        return dialect.transform(options.merge(kwargs)).map { |s| s % kwargs }
+      end
+    end
+  end
+end
---
+++ lib/bohu/command/dialect.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../command'
+
+# Config with dot access notation.
+class Bohu::Command::Dialect < Hash
+  autoload :Pathname, 'pathname'
+  autoload :YAML, 'yaml'
+
+  # @param [Hash] dict
+  def initialize(dict)
+    self.class.__send__(:symbolize_keys, dict || {}).each do |k, v|
+      self[k] = v
+    end
+  end
+
+  # Transform given hash.
+  #
+  # @param [Hash] input
+  # @return [Array<String>]
+  def transform(input)
+    [].tap do |output|
+      self.class.__send__(:symbolize_keys, input).each do |k, v|
+        if self.key?(k)
+          process_option(k, v, input)
+            .tap { |words| output.push(*words) }
+        else
+          output.push(v % input)
+        end
+      end
+    end
+  end
+
+  protected
+
+  # Translate given option to dialect.
+  #
+  # @param [Symbol] name
+  # @param [Boolean|String] value
+  # @param [Hash] input
+  def process_option(name, value, input = {})
+    trans = self.fetch(name)
+    vbool = [true, false].include?(trans[1])
+
+    return [trans[0], trans[1] % input] unless vbool
+    return [trans[0]] if value == trans[1]
+  end
+
+  class << self
+    def load(command, type)
+      paths.each do |path|
+        begin
+          path.join(*[type, command].map(&:to_s)).tap do |fname|
+            return file("#{fname}.yml")
+          end
+        rescue Errno::ENOENT => e
+          raise e if path == paths.last
+        end
+      end
+    end
+
+    # Get paths.
+    #
+    # @param [Bohu::Config] config
+    # @return [Array<Pathname>]
+    def paths(config = Bohu.config)
+      [Pathname.new("#{__dir__}/../dialects").realpath].tap do |paths|
+        (config[:dialect_paths] || []).reverse_each do |path|
+          paths.push(Pathname.new(path))
+        end
+      end.reverse
+    end
+
+    # @raise [Errno::ENOENT]
+    def file(file)
+      Pathname.new(file).realpath.read.tap do |content|
+        return self.new(YAML.safe_load(content))
+      end
+    end
+
+    protected
+
+    # Returns a new hash with all keys converted to symbols.
+    #
+    # @param [Hash] input
+    # @return [Hash]
+    def symbolize_keys(input)
+      Hash[input.map { |(k, v)| [k.to_s.to_sym, v] }]
+    end
+  end
+end
