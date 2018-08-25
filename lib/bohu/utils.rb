2d6cb5a1fb4050074dfc7d2d504153756998d3ae
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-25 16:39:25 +0200

    utils (lib) introduced

---
+++ lib/bohu/utils.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative '../bohu'
+require 'fileutils'
+
+# Provide several file utility methods for copying, moving, removing, etc.
+class Bohu::Utils
+  include Bohu::Configurable
+  include FileUtils
+
+  def initialize(*)
+    super
+
+    self.singleton_class.__send__(:include, FileUtils::Verbose) if verbose?
+
+    public_methods.each do |m|
+      # rubocop:disable Style/AccessModifierDeclarations
+      self.singleton_class.class_eval { public m }
+      # rubocop:enable Style/AccessModifierDeclarations
+    end
+  end
+
+  # @see Bohu::Shell.sh
+  def sh(*cmd)
+    Bohu::Shell.new(config_base).sh(*cmd)
+  end
+
+  # @return [Boolean]
+  def verbose?
+    return true unless config.key?(:verbose)
+    !!config[:verbose]
+  end
+
+  # @return [Array<Symbol>]
+  def public_methods
+    (super + [
+      %i[cd chdir chmod chmod_R chown chown_R cmp compare_file],
+      %i[compare_stream copy copy_entry copy_file copy_stream cp],
+      %i[cp_r getwd identical? install link ln ln_s ln_sf makedirs],
+      %i[mkdir mkdir_p mkpath move mv pwd remove remove_dir remove_entry],
+      %i[remove_entry_secure remove_file rm rm_f rm_r rm_rf rmdir rmtree],
+      %i[ruby safe_ln safe_unlink sh split_all symlink touch uptodate?],
+    ].flatten).sort
+  end
+end
