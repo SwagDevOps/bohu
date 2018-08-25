2cd0215ec8c8113101fbcf49d2041e642b58084d
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-25 17:45:41 +0200

    bohu_spec (spec) update + examples added

--- spec/bohu_spec.rb
+++ spec/bohu_spec.rb
@@ -12,6 +12,21 @@ describe Bohu, :bohu do
   let(:described_class) { Class.new { include Bohu } }
 
   it { expect(subject).to respond_to(:config).with(0).arguments }
-  it { expect(subject).to respond_to(:config).with(0).arguments }
+  it { expect(subject).to respond_to(:config_load).with(0).arguments }
+end
+
+describe Bohu, :bohu do
+  let(:described_class) { Class.new { include Bohu } }
+  let(:subject) do
+    described_class.new.tap do |instance|
+      instance.config_load(load_defaults: true)
+    end
+  end
+
   it { expect(subject).to respond_to(:sh).with_unlimited_arguments }
+
+  # FileUtils methods
+  sham!(:utils).im.each do |method|
+    it { expect(subject).to respond_to(method) }
+  end
 end
