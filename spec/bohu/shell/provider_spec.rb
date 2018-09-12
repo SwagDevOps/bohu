8cdeb35dcf1406ace5d4b42c58bc39b3541f52b9
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-09-12 13:28:41 +0200

    shell/provider_spec (spec) introduced

---
+++ spec/bohu/shell/provider_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# class methods
+describe Bohu::Shell::Provider, :'shell/provider' do
+  it { expect(described_class).to respond_to(:new).with(0).arguments }
+  it { expect(described_class).to respond_to(:new).with(1).arguments }
+end
+
+describe Bohu::Shell::Provider, :'shell/provider' do
+  it { expect(subject).to be_a(Bohu::Delegator) }
+  it { expect(subject).to be_a(Bohu::Configurable) }
+end
+
+# instance methods
+describe Bohu::Shell::Provider, :'shell/provider' do
+  let(:config) { { shell: { verbose: false } } }
+  let(:subject) { described_class.new(config) }
+
+  [:sh, :capture, :capture!].each do |method|
+    it { expect(subject).to respond_to(method).with_unlimited_arguments }
+  end
+end
