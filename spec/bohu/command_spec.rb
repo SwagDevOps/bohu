a2066e6087f6b5b189732a9f4c62c85165671dca
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-09-21 08:51:47 +0200

    command_spec (spec) examples added

--- spec/bohu/command_spec.rb
+++ spec/bohu/command_spec.rb
@@ -6,6 +6,12 @@ describe Bohu::Command, :command do
   it { expect(described_class).to respond_to(:new).with(3).arguments }
 end
 
+# const
+describe Bohu::Command, :command do
+  it { expect(described_class).to be_const_defined(:Runner) }
+  it { expect(described_class).to be_const_defined(:Dialect) }
+end
+
 # instance methods
 describe Bohu::Command, :command do
   let(:config) do
@@ -26,4 +32,14 @@ describe Bohu::Command, :command do
 
   it { expect(subject).to be_a(Array) }
   it { expect(subject).to be_a(Bohu::Configurable) }
+
+  it { expect(subject).to respond_to(:name).with(0).arguments }
+  it { expect(subject).to respond_to(:action).with(0).arguments }
+  it { expect(subject).to respond_to(:executable).with(0).arguments }
+  it { expect(subject).to respond_to(:executable?).with(0).arguments }
+  it { expect(subject).to respond_to(:prepare).with(0).arguments }
+  it { expect(subject).to respond_to(:prepare).with(1).arguments }
+  it { expect(subject).to respond_to(:preparable?).with(0).arguments }
+  it { expect(subject).to respond_to(:call).with(0).arguments }
+  it { expect(subject).to respond_to(:call).with(1).arguments }
 end
